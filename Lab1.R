7dbb6c84570e9662add581a18da3bdb3c9b69e12
SutherlandB <41080577+SutherlandB@users.noreply.github.com>
2025-01-24 14:23:09 -0500

    Create Lab1.R

---
+++ Lab1.R
@@ -0,0 +1,6 @@
+EPI_data <- read.csv("/Users/sutherlandbrown/Downloads/epi2024results06022024.csv") 
+setwd("/Users/sutherlandbrown/SouthVS/Data_Analytics/Labs")
+View(EPI_data)
+attach(EPI_data)
+EPI.new
+
